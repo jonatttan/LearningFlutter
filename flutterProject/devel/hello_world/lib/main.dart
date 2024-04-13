8eef8120b8652780beaacaadc673bba8d2c7cb0d
Jonattan Sousa <jonattan.sousa@gmail.com>
2024-04-13 12:17:14 -0300

    Lesson 36 - stateful widget: Creating a stateful widget, which is made up two objects, Widget and State. Widget store the imutable attributes, State store the mutables. We change the mutable variable  and refresh screen using  on the .

--- flutterProject/devel/hello_world/lib/main.dart
+++ flutterProject/devel/hello_world/lib/main.dart
@@ -1,32 +1,33 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  const int val = 10;
-
-  runApp(const MyApp(title: "My app title", value: val));
+  runApp(MyApp(name: 'Mohammed'));
 }
 
-class MyApp extends StatelessWidget {
-  final String title;
-  final int value;
+class MyApp extends StatefulWidget {
+  final String name;
+  const MyApp({Key? key, this.name = ''}) : super(key: key);
 
-  const MyApp({Key? key, this.title = '', this.value = 0}) : super(key: key);
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int salary = 2000;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            this.title,
-            style: TextStyle(color: Colors.black54),
-          ),
-        ),
-        body: Center(
-          child: Text(
-            'Hello World! The value is ' + this.value.toString(),
-            style: TextStyle(fontSize: 20, color: Colors.pinkAccent),
-          ),
+    return Center(
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            salary += 100;
+          });
+          print('salary: $salary');
+        },
+        child: Text(
+          'The ${widget.name} salary is, and salary $salary',
+          textDirection: TextDirection.ltr,
         ),
       ),
     );
