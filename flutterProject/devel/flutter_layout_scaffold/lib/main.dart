43ecdf2e08ca0e971b4debd2f56b1cbfafe389e0
Jonattan Sousa <jonattan.sousa@gmail.com>
2024-04-14 12:30:18 -0300

    Scaffold and AppBar - lesson 1: Using Scaffold (in PT andaime), set title and color on the AppBar and set body text on the body

---
+++ flutterProject/devel/flutter_layout_scaffold/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Application sample Scaffold'),
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
+          child: Text('My app'),
+        ),
+      ),
+    );
+  }
+}
